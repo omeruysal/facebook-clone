905ea5f0128e0cddb8d7d1ce3fffa07a3cc7d20e
Omer <teouysal@outlook.com>
2021-11-11 22:26:40 +0300

    created project, added nav bar

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Facebook</title>
+      </Head>
+      <Header/>
+    </div>
+  )
+}
